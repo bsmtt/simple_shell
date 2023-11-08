33066bfeeabca669e342310e51d24258437b6ea7
basmaebraheim <basmaebraheim@gmail.com>
2023-11-08 10:20:45 -0800

    add main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - start point to program
+ * @argc: number of arguments
+ * @argv: values of arguments
+ * @env: environment variables
+ * Return: 0 on succes.
+ */
+int main(int argc, char *argv[], char *env[])
+{
+	return (0);
+}
